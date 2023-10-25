5f4c3579f8ad5b2eb371bccb0b94c526e72e612e
MarcoCaamal <caamalmarco99@gmail.com>
2023-10-25 02:29:01 -0500

    Fix: Clean up unnecessary or unused web routes

--- routes/web.php
+++ routes/web.php
@@ -6,12 +6,4 @@ use Lib\Router;
 
 Router::get("/", [HomeController::class, "index"]);
 
-Router::get("/courses/:id/categories/:idCategory", function ($id, $idCategory) { 
-    return "El curso es $id con la categoria $idCategory";
-});
-
-Router::get("/courses/:id", function ($id) {
-    return "Courses Page con el id: $id";
- });
-
 Router::dispatch();
